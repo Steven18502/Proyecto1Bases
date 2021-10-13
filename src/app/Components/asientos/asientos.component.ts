246d02c534c9e0ada1f0e72f5603a5ff10cb2280
Steven <steven18502@gmail.com>
2021-10-13 04:07:51 -0600

    [AppWeb]
    *Se agrega la logica de los asientos
    *Se cambia el uso de archivos para las portadas de peliculas por URLs

--- src/app/Components/asientos/asientos.component.ts
+++ src/app/Components/asientos/asientos.component.ts
@@ -8,8 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AsientosComponent implements OnInit {
 
-  asientos:boolean[] = [true,false,false,true,false,false,false,false,false,true,false,false,true,false,false,true];
-  filas:string[] = ["1","2","3","4","5"];
+  asientos: Array<Array<number>> = [[0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0]]; 
+  asientosAux: Array<Array<number>> = [[0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0]]; 
   pelicula:string = "";
   cinema:string = "";
   sala:string = "";
@@ -17,12 +17,55 @@ export class AsientosComponent implements OnInit {
   constructor(private service:CinetecService) { }
 
   ngOnInit(): void {
+    this.restringir(70);
     this.service.cinemaAux.subscribe(c => {this.cinema = c});
-    console.log(this.cinema);
     this.service.peliculaAux.subscribe(p => {this.pelicula = p});
-    console.log(this.pelicula);
     this.service.salaAux.subscribe(s => {this.sala = s});
-    console.log(this.sala);
   }
+
+  restringir(porcentaje:number){
+    var fraccion:number = porcentaje/100;
+    var disponibles:number = Math.round(this.asientos[0].length*fraccion);
+    var restringidos:number = this.asientos[0].length - disponibles;
+    var cont:number = 0;
+    var dif:number = disponibles-restringidos;
+    var cant_cont:number = (1 + Math.trunc(disponibles/restringidos))
+    for (var i = 0; i<this.asientos.length; i++){
+      for(var j = 0; j<this.asientos[0].length; j++){
+        if (dif - (cant_cont - 1) < 0){
+          cant_cont = dif + 1;
+        }
+        if (dif <= 0){
+          cant_cont = 1;
+        }
+        if (cont == cant_cont){
+          this.asientos[i][j] = 2;
+          this.asientosAux[i][j] = 2;
+          cont = 0;
+          dif = dif - (cant_cont - 1);
+        }
+        else {
+          cont++;
+        }
+      };
+      cont = 0;
+      dif = disponibles-restringidos;
+      cant_cont = (1 + Math.trunc(disponibles/restringidos));
+    };
+
+  };
+
+  seleccionar(x:number,y:number){ 
+    if (this.asientosAux[x][y] == 0){
+      this.asientosAux[x][y] = 1;
+    }
+    else if (this.asientosAux[x][y] == 1){
+      this.asientosAux[x][y] = 0;
+    }
+  };
+
+  finalizar(){
+    this.asientos = this.asientosAux;
+  };
 
 }
